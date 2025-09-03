7c1e5f5b05226dd63aa594796dc465c74734227c
AriRice <97973849+AriRice@users.noreply.github.com>
2025-09-03 16:21:01 -0500

    Create 12.2.1_Dstats_1.sh

---
+++ 12_abba_baba/12.2.1_Dstats_1.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#SBATCH --chdir=./
+#SBATCH --job-name=ABBA-BABA_chrom
+#SBATCH --nodes=1 --ntasks=2
+#SBATCH --partition nocona
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=8G
+#SBATCH --array=1-34
+#SBATCH --mail-user=arrice@ttu.edu
+#SBATCH --mail-type=ALL
+
+workdir=/lustre/scratch/arrice/greenbul3/ABBA_BABA2/Dstats_by_chrom_Dsuite
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate phylostats_env
+
+chrom_array=$( head -n${SLURM_ARRAY_TASK_ID} scaffolds.txt | tail -n1 )
+
+~/Dsuite/Build/Dsuite Dtrios -n ${chrom_array} -t species_tree.nwk ${chrom_array}__ABBA_BABA.recode.vcf.gz Greenbul_Sets.txt -o ${workdir}/Greenbul_Sets

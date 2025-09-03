3701bd74b44e9e5ecb0cc87794316abd489ddccf
AriRice <97973849+AriRice@users.noreply.github.com>
2025-09-03 16:59:00 -0500

    Update 12.2_Dsuite.sh

--- 12_abba_baba/12.2_Dsuite.sh
+++ 12_abba_baba/12.2_Dsuite.sh
@@ -16,7 +16,7 @@ chrom_array1=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir1}/scaffolds.txt | tail -
 chrom_array2=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir2}/scaffolds.txt | tail -n1 )
 
 ~/Dsuite/Build/Dsuite Dtrios -n ${chrom_array1} -t ${workdir1}/species_tree_1.nwk ${chrom_array1}__ABBA_BABA.recode.vcf.gz \
-${workdir1}/Greenbul_Sets_1.txt -o ${workdir1}/Dstats_by_chrom_Dsuite/Greenbul_Sets
+${workdir1}/Greenbul_Sets_1.txt -o ${workdir1}/Dstats_by_chrom/Greenbul_Sets
 
 ~/Dsuite/Build/Dsuite Dtrios -n ${chrom_array2} -t ${workdir2}/species_tree_2.nwk ${chrom_array2}__ABBA_BABA.recode.vcf.gz \
-${workdir2}/Greenbul_Sets_2.txt -o ${workdir2}/Dstats_by_chrom_Dsuite/Greenbul_Sets
+${workdir2}/Greenbul_Sets_2.txt -o ${workdir2}/Dstats_by_chrom/Greenbul_Sets
